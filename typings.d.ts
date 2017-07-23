05e212e2405429ed06c949010c9f8d3b02cc9335
Justin Toon <jtoon@missiondata.com>
2017-07-23 12:26:37 -0400

    Initial commit

---
+++ typings.d.ts
@@ -0,0 +1,11 @@
+// Type definitions for Datasink 0.1.0 Project:
+// https://github.com/overneath42/innerface Definitions by: Justin Toon
+// <http://thewebdepartment.me> TypeScript Version: 2.4.2
+
+export namespace Global {
+
+}
+
+export namespace Datasink {
+
+}
