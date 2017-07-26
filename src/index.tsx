f1c789faebed24fde94cfd72beca7ba3d59a486a
Justin Toon <jtoon@missiondata.com>
2017-07-26 18:43:19 -0400

    Render null from component

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,10 @@ import { Datasink } from '../typings.d';
 
 import ds from './datasink.module';
 
-const Datasink = (props: Datasink.Props) => {
+const Datasink = (props: Datasink.Props): null => {
   ds(props).save();
 
-  return false;
+  return null;
 };
 
 export default Datasink;
