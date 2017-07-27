9f6f62f483d6ea6f1baac4dfb24cf96f761b7476
Justin Toon <jtoon@missiondata.com>
2017-07-26 22:18:08 -0400

    Early work at setting up hidden fields

--- src/datasink.module.ts
+++ src/datasink.module.ts
@@ -1,6 +1,42 @@
 import { Datasink } from '../typings.d';
+import { get, forEach } from 'lodash-es';
+import { createHiddenField, prepareData, decodeName } from './datasink.utils';
 
-export default function datasink(stateData: Datasink.StateData): { save(): void } {
+interface DatasinkModule {
+  save(): void;
+}
+
+export default function datasink(
+  stateData: Datasink.StateData
+): DatasinkModule {
+  /**
+   * Prepares a DocumentFragment with all necessary hidden fields
+   * to append to the form.
+   *
+   * @param {object} data The state data to store, saved as nested key-value pairs.
+   *
+   * @return {DocumentFragment} A fully prepared set of fields.
+   */
+  function prepareFields(data: Datasink.StateData): DocumentFragment {
+    let preparedFields = document.createDocumentFragment();
+
+    forEach(data, (field: Datasink.StateData) => {
+      if (field.fieldName) {
+        const dataToCommit = get(data, decodeName(field.fieldName));
+        const value = prepareData(dataToCommit);
+
+        preparedFields.appendChild(createHiddenField(field, value));
+      } else {
+
+      }
+    });
+
+    return preparedFields;
+  }
+
+  /**
+   * Saves the data!
+   */
   function save() {
     // TODO: something!
   }
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import { Datasink } from '../typings.d';
 
 import ds from './datasink.module';
 
-const Datasink = (props: Datasink.Props): null => {
+const Datasink = (props: Datasink.StateData): null => {
   ds(props).save();
 
   return null;
